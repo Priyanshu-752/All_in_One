9f79808edb302d85437975fd4638894d1239b133
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2023-09-18 20:39:20 +0530

    cut

--- src/Components/Price/CardComponent.jsx
+++ src/Components/Price/CardComponent.jsx
@@ -36,19 +36,6 @@ const CardComponent = () => {
                   <Card
                     image={image1}
                     Package={"Syllabus"}
-                    // Price={"$350"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-300"}
-                    // iconColor5={"text-green-300"}
-                    // iconColor6={"text-green-300"}
                     buttonBg={"bg-gray-300"}
                     hoverBgButton={"hover:bg-gray-200"}
                   />
@@ -57,19 +44,6 @@ const CardComponent = () => {
                   <Card
                     image={image2}
                     Package={"Previous Year Papers"}
-                    // Price={"$1200"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-500"}
-                    // iconColor5={"text-green-300"}
-                    // iconColor6={"text-green-300"}
                     buttonBg={"bg-green-400"}
                     hoverBgButton={"hover:bg-green-500"}
                     buttonTextColor={"text-white"}
@@ -81,19 +55,6 @@ const CardComponent = () => {
                   <Card
                     image={image3}
                     Package={"Research Papers"}
-                    // Price={"$7500"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-500"}
-                    // iconColor5={"text-green-500"}
-                    // iconColor6={"text-green-500"}
                     buttonBg={"bg-gray-300"}
                     hoverBgButton={"hover:bg-gray-200"}
                   />
@@ -116,19 +77,6 @@ const CardComponent = () => {
                   <Card
                     image={image1}
                     Package={"Syllabus"}
-                    // Price={"$350"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-300"}
-                    // iconColor5={"text-green-300"}
-                    // iconColor6={"text-green-300"}
                     buttonBg={"bg-gray-300"}
                     hoverBgButton={"hover:bg-gray-200"}
                   />
@@ -146,19 +94,6 @@ const CardComponent = () => {
                   <Card
                     image={image2}
                     Package={"Previous Year Papers"}
-                    // Price={"$1200"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-500"}
-                    // iconColor5={"text-green-300"}
-                    // iconColor6={"text-green-300"}
                     buttonBg={"bg-green-400"}
                     hoverBgButton={"hover:bg-green-500"}
                     buttonTextColor={"text-white"}
@@ -179,19 +114,6 @@ const CardComponent = () => {
                   <Card
                     image={image3}
                     Package={"Research Papers"}
-                    // Price={"$7500"}
-                    // Feature1={"Advanced Algorithmic Solutions"}
-                    // Feature2={"Trained Personnel Support"}
-                    // Feature3={"Innovative Technology Integration"}
-                    // Feature4={"Powerful Analytics Capabilities"}
-                    // Feature5={"Personalized Solutions"}
-                    // Feature6={"Continuous Innovation"}
-                    // iconColor1={"text-green-500"}
-                    // iconColor2={"text-green-500"}
-                    // iconColor3={"text-green-500"}
-                    // iconColor4={"text-green-500"}
-                    // iconColor5={"text-green-500"}
-                    // iconColor6={"text-green-500"}
                     buttonBg={"bg-gray-300"}
                     hoverBgButton={"hover:bg-gray-200"}
                   />
